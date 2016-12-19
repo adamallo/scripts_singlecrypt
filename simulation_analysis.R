c22a2357747da4187c3e9121c65741ff2e2cb9f7
Diego Mallo <dmalload@asu.edu>
2016-12-19 15:19:16 -0700

    Improvements in the analysis of simulation results and in the script to generate genotypes

--- simulation_analysis.R
+++ simulation_analysis.R
@@ -1,19 +1,123 @@
 library(cowplot)
-data=read.csv("/Users/Diego/Desktop/singlecrypt/simulation_nonprog/constant_infor.csv")
+library(grid)
+library(gridExtra)
+library(gtable)
+
+kind="nonprog"
+dir=paste0("/Users/Diego/Desktop/singlecrypt/simulation/final/simulation_",kind,"/results/")
+titletext="Nonprogressors (11 leaves)"
+data=read.csv(paste0(dir,"rf.csv"))
 data=data[,-1]
-data2=read.csv("/Users/Diego/Desktop/singlecrypt/simulation_nonprog/parameters.out",sep=" ")
+data2=read.csv(paste0(dir,"parameters.out"),sep=" ")
 data2_rel=data2
 data2_rel$cnv.loss=abs(data2$cnv.loss-1)
 data2_rel$cnv.conversion=abs(data2$cnv.conversion-1)
 data2_rel$clock.rate=abs(data2$clock.rate-data2$r)/data2$r
 
-rf_gd0=ggplot(data=data[data$gd==0,],aes(y=rf,x=as.factor(rate)))+geom_violin(aes(fill=mod))+scale_x_discrete(name="Mutation rate")+scale_y_continuous(name="RF distance")+labs(title="No whole-genome duplication")+scale_fill_discrete(name="Strategy")
-rf_gd=ggplot(data=data[data$gd=="0.001",],aes(y=rf,x=as.factor(rate)))+geom_violin(aes(fill=mod))+scale_x_discrete(name="Mutation rate")+scale_y_continuous(name="RF distance")+labs(title="Simulated whole-genome duplication")+scale_fill_discrete(name="Strategy")
-rf_r=ggplot(data=data[data$gd=="random",],aes(y=rf,x=as.factor(rate)))+geom_violin(aes(fill=mod))+scale_x_discrete(name="Mutation rate")+scale_y_continuous(name="RF distance")+labs(title="Random tip-only whole-genome duplication")+scale_fill_discrete(name="Strategy")
+#Mean bars inspired by aosmith16 @ github https://github.com/tidyverse/ggplot2/issues/1259 
+
+means=aggregate(formula= rf ~ rate+mod+gd,data=data,FUN=mean)
+
+rf_gd0=ggplot(data=data[data$gd==0,],aes(y=rf,x=as.factor(rate)))+
+  geom_violin(aes(fill=mod))+
+  geom_boxplot(aes(group=interaction(mod,as.factor(rate))),alpha=0,position=position_dodge(0.9),width=0.2)+
+  geom_errorbar(data=means[means$gd==0,],aes(y=rf,ymax=rf,ymin=rf,group=interaction(mod,as.factor(rate))),width=0.7,position=position_dodge(0.9),size=1,linetype=1)+
+  scale_x_discrete(name="Mutation rate")+
+  scale_y_continuous(name="RF distance")+
+  labs(title="No whole-genome duplication")+
+  scale_fill_discrete(name="Strategy")
+
+rf_gd=ggplot(data=data[data$gd=="0.001",],aes(y=rf,x=as.factor(rate)))+
+  geom_violin(aes(fill=mod))+
+  geom_boxplot(aes(group=interaction(mod,as.factor(rate))),alpha=0,position=position_dodge(0.9),width=0.2)+
+  geom_errorbar(data=means[means$gd=="0.001",],aes(y=rf,ymax=rf,ymin=rf,group=interaction(mod,as.factor(rate))),width=0.7,position=position_dodge(0.9),size=1,linetype=1)+
+  scale_x_discrete(name="Mutation rate")+
+  scale_y_continuous(name="RF distance")+
+  labs(title="Simulated whole-genome duplication")+
+  scale_fill_discrete(name="Strategy")
+
+rf_r=ggplot(data=data[data$gd=="random",],aes(y=rf,x=as.factor(rate)))+
+  geom_violin(aes(fill=mod))+geom_boxplot(aes(group=interaction(mod,as.factor(rate))),alpha=0,position=position_dodge(0.9),width=0.2)+
+  geom_errorbar(data=means[means$gd=="random",],aes(y=rf,ymax=rf,ymin=rf,group=interaction(mod,as.factor(rate))),width=0.7,position=position_dodge(0.9),size=1,linetype=1)+
+  scale_x_discrete(name="Mutation rate")+scale_y_continuous(name="RF distance")+
+  labs(title="Random tip-only whole-genome duplication")+
+  scale_fill_discrete(name="Strategy")
+
 grid_rf=plot_grid(rf_gd0,rf_gd,rf_r,labels="AUTO",ncol=3)
-rate_gd0=ggplot(data=data2_rel[data2_rel$gd==0,],aes(y=clock.rate,x=as.factor(r)))+geom_violin(aes(fill=mod))+scale_x_discrete(name="Mutation rate")+scale_y_log10(name="Mutation rate relative error")+labs(title="No whole-genome duplication")+scale_fill_discrete(name="Strategy")
-rate_gd=ggplot(data=data2_rel[data2_rel$gd=="0.001",],aes(y=clock.rate,x=as.factor(r)))+geom_violin(aes(fill=mod))+scale_x_discrete(name="Mutation rate")+scale_y_log10(name="Mutation rate relative error")+labs(title="Simulated whole-genome duplication")+scale_fill_discrete(name="Strategy")
-rate_r=ggplot(data=data2_rel[data2_rel$gd=="random",],aes(y=clock.rate,x=as.factor(r)))+geom_violin(aes(fill=mod))+scale_x_discrete(name="Mutation rate")+scale_y_log10(name="Mutation rate relative error")+labs(title="Random tip-only whole-genome duplication")+scale_fill_discrete(name="Strategy")
-grid_rate=plot_grid(rate_gd0,rate_gd,rate_r,labels="AUTO",ncol=3)
+
+
+means=aggregate(formula= clock.rate ~ r+mod+gd,data=data2_rel,FUN=mean)
+
+rate_gd0=ggplot(data=data2_rel[data2_rel$gd==0,],aes(y=clock.rate,x=as.factor(r)))+
+  geom_violin(aes(fill=mod))+geom_boxplot(aes(group=interaction(mod,as.factor(r))),alpha=0,position=position_dodge(0.9),width=0.2)+
+  geom_errorbar(data=means[means$gd==0,],aes(y=clock.rate,ymax=clock.rate,ymin=clock.rate,group=interaction(mod,as.factor(r))),width=0.7,position=position_dodge(0.9),size=1,linetype=1)+
+  scale_x_discrete(name="Mutation rate")+
+  scale_y_log10(name="Mutation rate relative error",limits=c(min(data2_rel$clock.rate),max(data2_rel$clock.rate)))+
+  labs(title="No whole-genome duplication")+
+  scale_fill_discrete(name="Strategy")
+
+rate_gd=ggplot(data=data2_rel[data2_rel$gd=="0.001",],aes(y=clock.rate,x=as.factor(r)))+
+  geom_violin(aes(fill=mod))+geom_boxplot(aes(group=interaction(mod,as.factor(r))),alpha=0,position=position_dodge(0.9),width=0.2)+
+  geom_errorbar(data=means[means$gd=="0.001",],aes(y=clock.rate,ymax=clock.rate,ymin=clock.rate,group=interaction(mod,as.factor(r))),width=0.7,position=position_dodge(0.9),size=1,linetype=1)+
+  scale_x_discrete(name="Mutation rate")+
+  scale_y_log10(name="Mutation rate relative error",limits=c(min(data2_rel$clock.rate),max(data2_rel$clock.rate)))+
+  labs(title="Simulated whole-genome duplication")+
+  scale_fill_discrete(name="Strategy")
+
+rate_r=ggplot(data=data2_rel[data2_rel$gd=="random",],aes(y=clock.rate,x=as.factor(r)))+
+  geom_violin(aes(fill=mod))+geom_boxplot(aes(group=interaction(mod,as.factor(r))),alpha=0,position=position_dodge(0.9),width=0.2)+
+  geom_errorbar(data=means[means$gd=="random",],aes(y=clock.rate,ymax=clock.rate,ymin=clock.rate,group=interaction(mod,as.factor(r))),width=0.7,position=position_dodge(0.9),size=1,linetype=1)+
+  scale_x_discrete(name="Mutation rate")+
+  scale_y_log10(name="Mutation rate relative error",limits=c(min(data2_rel$clock.rate),max(data2_rel$clock.rate)))+
+  labs(title="Random tip-only whole-genome duplication")+
+  scale_fill_discrete(name="Strategy")
+
+grid_rate=plot_grid(rate_gd0,rate_gd,rate_r,labels=c("D","E","F"),ncol=3)
+
 supergrid=plot_grid(grid_rf,grid_rate,labels="",ncol=1)
-save_plot("results_inforNe.pdf",supergrid,base_height=12,base_aspect_ratio=1.5)
+supergrid_gtable=cowplot:::ggplot_to_gtable(supergrid)
+
+#By baptiste @ Stackoverflow: http://stackoverflow.com/questions/31640916/how-can-i-add-a-title-to-a-tablegrob-plot
+titlegrob = textGrob(titletext,gp=gpar(fontsize=25,fontface="bold"))
+padding <- unit(7,"mm")
+supergrid_gtable <- gtable_add_rows(supergrid_gtable, 
+                         heights = grobHeight(titlegrob) + padding,
+                         pos = 0)
+supergrid_gtable <- gtable_add_grob(supergrid_gtable, titlegrob, 1, 1, 1, ncol(supergrid_gtable))
+##
+
+finalplot=ggdraw(supergrid_gtable)
+save_plot(paste0(dir,"panel_",kind,".pdf"),finalplot,base_height=12,base_aspect_ratio=1.5)
+save_plot(paste0(dir,"panel_",kind,".png"),finalplot,base_height=12,base_aspect_ratio=1.5)
+
+methods=unique(data$mod)
+n_comb=length(methods)*(length(methods)-1)/2 #Bonferroni correction
+
+for (dup in unique(data$gd)) {
+  for (rate in unique(data$rate)) {
+    for (a in 1:(length(methods)-1)){
+      for (b in (a+1):length(methods)){
+        datA=data[data$gd==dup & data$rate==rate & data$mod==methods[a],]$rf
+        datB=data[data$gd==dup & data$rate==rate & data$mod==methods[b],]$rf
+        if(length(datA)>length(datB)) {
+          datA=#get again the data, only with the replicates present in B
+        }else if (length(datB)>length(datA)) {
+          datB=#get again the data, only with the replicates present in A
+        }
+        
+        p_value=wilcox.test(datA,datB,paired = TRUE)$p.value
+        add=ifelse(p_value > 0.05/n_comb, "", ifelse(p_value > 0.01/n_comb, " *",
+                                                     ifelse(p_value > 0.001/n_comb, " **", " ***")))
+        print(paste(paste("tTop",dup,rate,methods[a],methods[b],sep=":"),paste0(signif(p_value,digits=2),add),sep=","))
+
+        datA=data2_rel[data2_rel$gd==dup & data2_rel$r==rate & data2_rel$mod==methods[a],]$clock.rate
+        datB=data2_rel[data2_rel$gd==dup & data2_rel$r==rate & data2_rel$mod==methods[b],]$clock.rate
+        
+        p_value=wilcox.test(datA,datB,paired = TRUE)$p.value
+        add=ifelse(p_value > 0.05/n_comb, "", ifelse(p_value > 0.01/n_comb, " *",
+                                                     ifelse(p_value > 0.001/n_comb, " **", " ***")))
+        print(paste("mRate",dup,rate,methods[a],methods[b],sep=":"))
+      }
+    }
+  }
+} 
