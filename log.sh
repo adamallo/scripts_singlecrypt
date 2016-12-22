38cadcd389987cf7e5b10ae392a88e1ef89c97b5
Diego Mallo <dmalload@asu.edu>
2016-12-22 13:14:55 -0700

    Added code to recover incompleted BEAST runs in the log.sh file

--- log.sh
+++ log.sh
@@ -5,4 +5,7 @@ mkdir results;mv r*/* results/;mkdir results/MCC;mv results/*MCC* results/MCC
 mkdir results/out; mv results/*.out results/out
 rmdir r*
 for i in $(ls results/*.trees | xargs -n1 basename | sed "s/\.r.\.trees//" | uniq | sort);do mkdir results/$i; for j in results/$i*; do if [[ -f $j ]];then mv $j results/$i;fi;done;done
-for i in *phased*; do sbatch Rscript.sbatch ../../scripts_singlecrypt/rwty.R $i ;done
+for i in *phased*; do sbatch -c 3 Rscript.sbatch ../../../../scripts_singlecrypt/rwty.R $i 5 3;done
+
+##Incompleted runs with dump_states
+for i in *.xml ; do name=$(head $i | tail -n 1 | sed 's/<.*\"\(.*\)\">/\1/g');file=$(grep -l $name * | tail -n1);echo sbatch beast.sbatch -seed 25 -dump_every 10000000 -load_dump $file $i;done
