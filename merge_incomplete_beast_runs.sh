25671f3a9fcf9dee57080244b0314513e4c081f2
Diego Mallo <dmalload@asu.edu>
2017-01-02 21:18:37 -0700

    Improvements in the script to merge recovered runs

--- merge_incomplete_beast_runs.sh
+++ merge_incomplete_beast_runs.sh
@@ -1,12 +1,14 @@
 ##Putting incompleted runs together
 
-if [[ ${#ARGV[*]} -ne 1 ]]
+if [[ $# -ne 1 ]]
 then
     echo "Usage: $0 directory"
     exit
 fi
 
 cd $1
+
+eliminate_extra=500050000
 
 for i in *.xml
     do
@@ -25,14 +27,30 @@ for i in *.xml
         for j in $name.log*
         do
             stop=${states[$(($istate + 1 ))]}
+            if [[ $eliminate_extra != "" ]] && [[ $stop -eq "" ]]
+            then
+                stop=$eliminate_extra
+            fi
+            
+            if [[ $stop -eq "" ]]
+            then
+                stop_tree=""
+            else
+                stop_tree="STATE_$stop"
+            fi
+
             echo $istate $j $stop
+
+            id=$(echo $j | sed "s/$name.log_//")
+
             if [[ $istate -eq 0 ]]
             then
-               awk -v stop=$stop '{if ($1 != stop ) {print $0} else {exit}}' $j > $name.test
+               awk -v stop=$stop '{if ($1 != stop ) {print $0} else {exit}}' $j > $name.merged.log
+               awk -v stop=$stop_tree '{if ($2 != stop ) {print $0} else {exit}}' $name.trees_$id > $name.merged.trees
             else
-                tail -n +4 $j | awk -v stop=$stop '{if ($1 != stop ) {print $0} else {exit}}' >> $name.test
+                tail -n +4 $j | awk -v stop=$stop '{if ($1 != stop ) {print $0} else {exit}}' >> $name.merged.log
+                awk -v stop=$stop_tree 'BEGIN{skip=1}{if(skip==1 && $1 == "tree"){skip=0} if (skip==0){if($2 != stop ) {print $0} else {exit}}}' $name.trees_$id >> $name.merged.trees
             fi
             istate=$(( $istate + 1 )) 
         done
-
 done
